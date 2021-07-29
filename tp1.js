7abe8b386e4303de70a96ea662b96b6f3b61f11e
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-07-29 10:58:18 +0100

    javascript native

---
+++ tp1.js
@@ -0,0 +1,23 @@
+
+
+function info(){
+
+    document.writeln("Plan du cours : Javascript, jQuery, Ajax");
+}
+
+
+function affichage()
+{
+    alert("Bienvenue dans le monde JS");
+}
+
+
+function debut()
+{
+    alert("Départ");
+}
+
+function fin()
+{
+    alert("Arrivé");
+}
